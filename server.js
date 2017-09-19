ff88c623820e71870916f78b61a83fbc55f80952
Jacob Thomas <jcthomas00@gmail.com>
2017-09-18 19:36:03 -0500

    initial server commit

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var mysql = require("mysql");
 var app = express();
 var PORT = process.env.PORT||3000;
 
+app.get("/", function(req, res){
+	res.send("Listening on port ${PORT}");
+})
+
 app.listen(PORT, function(){
-	
+
 });
