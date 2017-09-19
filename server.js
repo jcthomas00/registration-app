fd4623247b92231a664177dbb1b5a073f0710d3f
Jacob Thomas <jcthomas00@gmail.com>
2017-09-18 19:46:26 -0500

    api setup

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ var mysql = require("mysql");
 var app = express();
 var PORT = process.env.PORT||3000;
 
+var reservations = [{
+	name: "jacob",
+	phone: "281-222-2222",
+	email: "jacob@gg.com",
+	id: 12344
+}];
+
 app.get("/", function(req, res){
-	res.send("Listening on port ${PORT}");
+	res.send(`Listening on port ${PORT}`);
+})
+
+app.get("/api/tables", function(req, res){
+	res.json(reservations);
 })
 
 app.listen(PORT, function(){
-
+	console.log(`Listening on port ${PORT}`);
 });
